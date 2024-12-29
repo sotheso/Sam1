8ea5f1a1aff30a07c5daee558ce5c98d6521eda5
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-29 10:57:29 +0330

    Initial Commit

---
+++ Sam1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sam1
+//
+//  Created by Sothesom on 09/10/1403.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
